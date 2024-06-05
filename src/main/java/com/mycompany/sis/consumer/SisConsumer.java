f1b0f426606338542e3f906d74b9bf148a3236da
Lucaslllll <lucascosta12367@gmail.com>
2024-06-05 16:55:06 -0300

    init

---
+++ src/main/java/com/mycompany/sis/consumer/SisConsumer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sis.consumer;
+
+/**
+ *
+ * @author Lucas
+ */
+public class SisConsumer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
